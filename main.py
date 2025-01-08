cf30d182b1eee53ec5a4a0480a1cdfe15a44e806
Sushrut <xboxbedrock@gmail.com>
2025-01-07 23:43:16 -0800

    drive control

--- main.py
+++ main.py
@@ -2,14 +2,32 @@ import evdev
 import sys
 import time
 import util
+import RPi.GPIO as gpio
+
+lport = 2
+rport = 3
+
+#gpio init stuff
+gpio.setmode(gpio.BCM)
+gpio.setup(lport, gpio.OUT)
+gpio.setup(rport, gpio.OUT)
+
+#50hz standard
+lpwm = gpio.PWM(lport ,50)
+rpwm = gpio.PWM(rport, 50)
+
+#neutral position
+lpwm.start(50)
+rpwm.start(50)
+
 
 controller_name = "Xbox Wireless Controller"
-
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 
 found_path = None
 
 for device in devices:
+    print(device.name)
     if device.name == controller_name:
         found_path = device.path
         
@@ -21,6 +39,11 @@ controller = evdev.InputDevice(found_path)
 print(controller.capabilities(verbose=True, absinfo=True))
 #read events
 while True:
-    print(util.clean_input_127(controller.absinfo(evdev.ecodes.ABS_X)))
-    print(controller.active_keys(verbose=True))
+    x_val = util.clean_input_127(controller.absinfo(evdev.ecodes.ABS_X))
+    y_val = util.clean_input_127(controller.absinfo(evdev.ecodes.ABS_Y))
+    left = y_val - x_val
+    right = y_val + x_val
+    rpwm.ChangeDutyCycle(util.pwm_ify(right))
+    lpwm.ChangeDutyCycle(util.pwm_ify(left))
+    print(util.is_button_pressed('A', controller))
     time.sleep(0.5)
--- util.py
+++ util.py
@@ -1,5 +1,19 @@
 import evdev
 
+button_maps = {
+    307: "X",
+    308: "Y",
+    304: "A",
+    305: "B"
+    }
+
+def pwm_ify(val: int):
+    val = min(val, 127)
+    val = max(val, -127)
+    val += 127
+    val *= 100/254
+    return int(val)
+
 #clean the reading and scale to -127 to +127
 def clean_input_127(device_reading: evdev.device.AbsInfo):
     if abs(device_reading.value) < device_reading.flat:
@@ -12,3 +26,9 @@ def clean_input_127(device_reading: evdev.device.AbsInfo):
     val *= 127/abs(device_reading.min)
     
     return int(val) if (val < 0) else int(val+0.5)
+
+def is_button_pressed(name: str, device: evdev.InputDevice):
+    pressed = [button_maps.get(i) for i in device.active_keys()]
+    return name in pressed
+    
+    
